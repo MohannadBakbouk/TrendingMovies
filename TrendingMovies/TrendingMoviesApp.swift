01a72e756d3a4be14ca2fc5fea2a2ca9ef34ee90
MohannadBakbouk <MohannadBakBouk@gmail.com>
2026-04-11 02:02:32 +0200

    Initial Commit

---
+++ TrendingMovies/TrendingMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendingMoviesApp.swift
+//  TrendingMovies
+//
+//  Created by Mohannad on 11/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TrendingMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
